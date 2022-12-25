e5b9157a9d03d70d2cdaad18160ed268c019e69e
Jakob Buis <jakob@jakobbuis.nl>
2022-12-25 11:37:45 +0100

    configure deployer from bolknoms

--- deploy.php
+++ deploy.php
@@ -3,11 +3,23 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
+set('application', 'rsvp');
 set('repository', 'git@github.com:jakobbuis/rsvp.git');
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+set('keep_releases', 5);
 
 host('jakobbuis.nl')
     ->set('remote_user', 'jakob')
-    ->set('deploy_path', '/srv/rsvp');
+    ->set('deploy_path', '/srv/rsvp')
+    ->set('branch', 'main');
+
+// Build frontend upon release
+task('build:frontend', function () {
+    within('{{release_path}}', function () {
+        run('npm install');
+        run('npm run production');
+    });
+});
+before('deploy:publish', 'build:frontend');
+
+// Always migrate on deployment
+before('deploy:symlink', 'artisan:migrate');

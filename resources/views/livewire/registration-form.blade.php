450d051f6f1d74793e176e88a847d75e6b9c705f
Jakob Buis <jakob@jakobbuis.nl>
2022-12-27 16:09:10 +0100

    show UI as clearly disabled when deadline is passed

--- resources/views/livewire/registration-form.blade.php
+++ resources/views/livewire/registration-form.blade.php
@@ -1,9 +1,31 @@
 <div class="bg-blue-100 p-4 rounded-lg shadow-md mb-4">
-    <h3 class="font-bold mb-2">RSVP</h3>
+    <h3 class="font-bold mb-2">
+        RSVP
+        @if (!$event->openForRegistrations())
+            <span class="text-red-600">
+                {{-- icon exclamation --}}
+                <svg class="w-6 inline-block ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+                </svg>
+                Deadline verstreken
+            </span>
+        @endif
+    </h3>
     <form wire:submit.prevent="register">
         <p>
-            <input type="text" wire:model="name" class="text-lg rounded-md p-2 mb-2 border-2 w-full" placeholder="Jouw naam">
-            <button type="submit" class="text-lg p-2 rounded-md border-1 border-blue-900 bg-blue-300 w-full inline-block">RSVP</button>
+            <input
+                type="text"
+                wire:model="name"
+                placeholder="Jouw naam"
+                class="text-lg rounded-md p-2 mb-2 border-2 w-full"
+                {{ !$event->openForRegistrations() ? 'disabled' : null }}>
+
+                <button
+                    type="submit"
+                    {{ !$event->openForRegistrations() ? 'disabled' : null }}
+                    class="text-lg p-2 rounded-md border-1 border-blue-900 bg-blue-300 w-full inline-block">
+                        RSVP
+                </button>
         </p>
     </form>
 

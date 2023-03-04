adad258f53b92b9b1d8845ab3dc8d75b98aed757
Jakob Buis <jakob@jakobbuis.nl>
2023-03-04 19:31:35 +0100

    remove obsolete login link behaviour

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,3 @@
 import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-const signin = document.querySelector('#sign-in');
-if (signin) {
-    signin.addEventListener('click', event => {
-        event.preventDefault();
-        alert('Coming soon');
-    });
-}

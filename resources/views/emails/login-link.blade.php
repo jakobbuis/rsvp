2b867d019b17042218bc6c9f0fbf5474dfa87164
Jakob Buis <jakob@jakobbuis.nl>
2023-03-04 19:40:21 +0100

    fix link text

--- resources/views/emails/login-link.blade.php
+++ resources/views/emails/login-link.blade.php
@@ -4,7 +4,7 @@
 Gebruik de button hieronder om je aan te melden in RSVP. Als je nog geen account hebt, wordt er automatisch een voor je aangemaakt.
 
 <x-mail::button :url="$link">
-Sign in to RSVP
+Login bij RSVP
 </x-mail::button>
 
 Deze link is 15 minuten geldig en verloopt op {{ $validUntil->toTimeString() }}.
